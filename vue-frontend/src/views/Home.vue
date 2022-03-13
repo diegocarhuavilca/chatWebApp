de540b09f6cc3fffee96c7e60b50d1d6a343c8c5
Diego.Carhuavilca <diego.carhuavilca@gmail.com>
2022-03-13 13:00:03 -0500

    [frontend] initial vue frontend

---
+++ vue-frontend/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <n-button type="warning">
+      Warning
+    </n-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+ import { NButton } from 'naive-ui'
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld,
+    NButton
+  },
+});
+</script>
